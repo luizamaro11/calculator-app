8de7e5cf9c47b52add5d48407a05707a45714005
luizamaro11 <luiz.amaro90@gmail.com>
2023-03-11 17:53:35 -0300

    Projeto finalizado com calculadora e troca de tema

--- src/js/script.js
+++ src/js/script.js
@@ -52,12 +52,53 @@ for (i = 0; i < operations.length; i++) {
 
 let equal = document.querySelector(".equal_to");
 
-equal.addEventListener("click", function() {
+equal.addEventListener("click", resultCalculator);
 
+let reset = document.querySelector(".reset");
+
+reset.addEventListener('click', function() {
+    inputResult.innerHTML = "";
+});
+
+window.addEventListener("keydown", (event) => {
+
+    let keydownPermission = ['+', '-', '*', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+
+    if (keydownPermission.includes(event.key)) {
+        inputResult.innerHTML += event.key;
+    }
+
+    if (event.key === 'Backspace') {
+        inputResult.innerHTML = inputResult.textContent.slice(0, -1);
+
+        if (inputResult.textContent === '') {
+            inputResult.innerHTML = '0';
+        }
+    }
+
+    if (event.key === 'Enter') {
+        resultCalculator();
+    }
+
+    if (event.ctrlKey === true && event.key === 'Backspace') {
+        inputResult.innerHTML = '0';
+    }
+}, true);
+
+let del = document.querySelector(".button_del");
+
+del.addEventListener('click', function() {
+    inputResult.innerHTML = inputResult.textContent.slice(0, -1);
+
+    if (inputResult.textContent === '') {
+        inputResult.innerHTML = '0';
+    }
+});
+
+function resultCalculator() {
     let input = inputResult.textContent;
     let regexValueOne;
     let regexValueTwo;
-
 
     const regex = /./gm;
     let typeNumber = input.match(regex);
@@ -115,34 +156,7 @@ equal.addEventListener("click", function() {
 
         inputResult.innerHTML = result.toString();
     }
-})
-
-let reset = document.querySelector(".reset");
-
-reset.addEventListener('click', function() {
-    inputResult.innerHTML = "";
-});
-
-window.addEventListener("keydown", (event) => {
-
-    if (event.key.includes([''])) {
-
-    }
-    inputResult.innerHTML += event.key;
-    var resultArr = indexArr.map(i => fruitier[i]);
-
-    console.log(event);
-}, true);
-
-let del = document.querySelector(".button_del");
-
-del.addEventListener('click', function() {
-    inputResult.innerHTML = inputResult.textContent.slice(0, -1);
-
-    if (inputResult.textContent === '') {
-        inputResult.innerHTML = '0';
-    }
-});
+}
 
 function calculator(valueOne, valueTwo, operation) {
 
@@ -191,4 +205,78 @@ function getSignalOperator(operator) {
     }
 
     return strOperator;
+}
+
+// troca de tema
+const theme = document.querySelector('[name="theme"]');
+const root = document.querySelector(':root');
+
+theme.addEventListener("change", function(e) {
+    const valueTheme = e.target.value;
+    alterTheme(valueTheme);
+});
+
+function alterTheme(valueTheme) {
+
+    // --background-body: hsl(222, 26%, 31%);
+    // --color-text: #fff;
+    // --background-result: hsl(224, 36%, 15%);
+    // --background-calculator: hsl(223, 31%, 20%);
+    // --background-button: hsl(30, 25%, 89%);
+    // --shadow-button: hsl(28, 16%, 65%);
+    // --background-button-del: hsl(225, 21%, 49%);
+    // --background-button-reset: hsl(225, 21%, 49%);
+    // --shadow-button-del: hsl(224, 28%, 35%);
+    // --shadow-button-reset: hsl(224, 28%, 35%);
+    // --background-button-equal: hsl(6, 63%, 50%);
+    // --shadow-button-equal: hsl(6, 70%, 34%);
+
+    console.log(root, valueTheme);
+
+    if (valueTheme == 1) {
+        root.style.setProperty('--background-body', 'hsl(222, 26%, 31%)');
+        root.style.setProperty('--color-text', '#fff');
+        root.style.setProperty('--background-result', 'hsl(224, 36%, 15%)');
+        root.style.setProperty('--background-calculator', 'hsl(223, 31%, 20%)');
+        root.style.setProperty('--background-button', 'hsl(30, 25%, 89%)');
+        root.style.setProperty('--color-text-button', 'hsl(60, 10%, 19%)');
+        root.style.setProperty('--shadow-button', 'hsl(28, 16%, 65%)');
+        root.style.setProperty('--background-button-del', 'hsl(225, 21%, 49%)');
+        root.style.setProperty('--background-button-reset', 'hsl(225, 21%, 49%)');
+        root.style.setProperty('--shadow-button-del', 'hsl(224, 28%, 35%)');
+        root.style.setProperty('--shadow-button-reset', 'hsl(224, 28%, 35%)');
+        root.style.setProperty('--background-button-equal', 'hsl(6, 63%, 50%)');
+        root.style.setProperty('--shadow-button-equal', 'hsl(6, 70%, 34%)');
+        root.style.setProperty('--color-text-equal', '#fff');
+    } else if (valueTheme == 2) {
+        root.style.setProperty('--background-body', 'hsl(0, 0%, 90%)');
+        root.style.setProperty('--color-text', 'hsl(60, 10%, 19%)');
+        root.style.setProperty('--background-result', 'hsl(0, 0%, 93%)');
+        root.style.setProperty('--background-calculator', 'hsl(0, 5%, 81%)');
+        root.style.setProperty('--background-button', 'hsl(45, 7%, 89%)');
+        root.style.setProperty('--color-text-button', 'hsl(60, 10%, 19%);');
+        root.style.setProperty('--shadow-button', 'hsl(35, 11%, 61%)');
+        root.style.setProperty('--background-button-del', 'hsl(185, 42%, 37%)');
+        root.style.setProperty('--background-button-reset', 'hsl(185, 42%, 37%)');
+        root.style.setProperty('--shadow-button-del', 'hsl(185, 58%, 25%)');
+        root.style.setProperty('--shadow-button-reset', 'hsl(185, 58%, 25%)');
+        root.style.setProperty('--background-button-equal', 'hsl(25, 98%, 40%)');
+        root.style.setProperty('--shadow-button-equal', 'hsl(25, 99%, 27%)');
+        root.style.setProperty('--color-text-equal', '#fff');
+    } else if (valueTheme == 3) {
+        root.style.setProperty('--background-body', 'hsl(268, 75%, 9%)');
+        root.style.setProperty('--color-text', 'hsl(52, 100%, 62%)');
+        root.style.setProperty('--background-result', 'hsl(268, 71%, 12%)');
+        root.style.setProperty('--background-calculator', 'hsl(268, 71%, 12%)');
+        root.style.setProperty('--background-button', 'hsl(268, 47%, 21%)');
+        root.style.setProperty('--color-text-button', 'hsl(52, 100%, 62%)');
+        root.style.setProperty('--shadow-button', 'hsl(290, 70%, 36%)');
+        root.style.setProperty('--background-button-del', 'hsl(281, 89%, 26%)');
+        root.style.setProperty('--background-button-reset', 'hsl(281, 89%, 26%)');
+        root.style.setProperty('--shadow-button-del', 'hsl(285, 91%, 52%)');
+        root.style.setProperty('--shadow-button-reset', 'hsl(285, 91%, 52%)');
+        root.style.setProperty('--background-button-equal', 'hsl(176, 100%, 44%)');
+        root.style.setProperty('--shadow-button-equal', 'hsl(177, 92%, 70%)');
+        root.style.setProperty('--color-text-equal', '#000');
+    }
 }
